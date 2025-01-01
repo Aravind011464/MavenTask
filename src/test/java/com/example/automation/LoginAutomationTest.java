63abae9a0ddc7115dc0365118278b5a3d7f11464
Aravind <aravindah03011@gmail.com>
2025-01-01 19:36:14 +0530

    first commit

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.Duration;
 
 public class LoginAutomationTest {
     @Test
@@ -14,11 +17,14 @@ public class LoginAutomationTest {
         WebDriver driver = new ChromeDriver();
 
         try {
-            driver.get("https://example.com/login");
+            driver.get("http://localhost:3000/");
 
-            WebElement usernameField = driver.findElement(By.id("username"));
-            WebElement passwordField = driver.findElement(By.id("password"));
-            WebElement loginButton = driver.findElement(By.id("loginButton"));
+            // Use WebDriverWait to wait until the elements are visible
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
+            WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+            WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("loginButton")));
 
             usernameField.sendKeys("testUser");
             passwordField.sendKeys("testPassword");
